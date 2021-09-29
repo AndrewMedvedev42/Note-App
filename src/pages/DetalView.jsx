8d2a538bcc994b7dcb8edeb3392a0bf1eb86cd04
andrewNEW42 <sanjino56@gmail.com>
2021-09-29 12:26:49 +0300

    managed to update note title and value seperately by updating title on change, and value by on click on button

--- src/pages/DetalView.jsx
+++ src/pages/DetalView.jsx
@@ -6,15 +6,14 @@ import { useEffect, useState } from "react";
 export const DetalView = () => {
 
     const [note, setNote] = useState({})
-    const [noteTitle, setNoteTitle] = useState("")
+    // const [noteTitle, setNoteTitle] = useState("")
     const [noteValue, setNoteValue] = useState("")
 
-    const setTitle = (e) => {
-        setNoteTitle(e.target.value)
-}
+    const [ saveButtonStatus, setSaveButtonStatus] = useState(true)
 
     const setValue = (e) => {
         setNoteValue(e.target.value)
+        setSaveButtonStatus(false)
 }
 
     const location = useLocation()
@@ -39,19 +38,23 @@ export const DetalView = () => {
         document.execCommand("copy")
     }
 
-    const updateNote = () => {
-        fireRef.child(noteID).update({"title": noteTitle,'value': noteValue})
+    const updateNoteValue = () => {
+        fireRef.child(noteID).update({'value': noteValue})
+    }
+
+    const updateNoteTitle = (e) => {
+        fireRef.child(noteID).update({'title': e.target.value})
     }
 
     const history = useHistory()
     return (<section className="detal-view-section">
                 <div className="detal-view-container">   
                     <div className="edit-section">
-                        <input className="title-textarea" defaultValue={note.title} placeholder="Title" onChange={setTitle}/>
+                        <input className="title-textarea" defaultValue={note.title} placeholder="Title" onChange={updateNoteTitle}/>
                         <textarea className="value-textarea" placeholder="You can write here..." defaultValue={note.value} onChange={setValue}></textarea>
                     </div>
                     <div className="button-container">
-                        <button onClick={updateNote}>Save</button>
+                        <button onClick={updateNoteValue} disabled={saveButtonStatus}>Save</button>
                         <button onClick={copyText}>Copy Text</button>
                         <button>Delete</button>
                         <button onClick={()=>{history.push("/")}}>Back</button>
